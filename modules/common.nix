1866cae3da575f8f44d741eb83047d82ad911996
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2019-03-15 11:59:31 +0100

    openpyxl

--- modules/common.nix
+++ modules/common.nix
@@ -22,6 +22,7 @@ in {
       # List packages installed in system profile. To search, run:
       # $ nix search wget
       environment.systemPackages = with pkgs; [
+        python36Packages.openpyxl
         python36Packages.pylint
         perlPackages.TextIconv
         libreoffice
