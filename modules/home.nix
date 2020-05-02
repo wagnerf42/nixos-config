5534cb66edf7cd740f1e2d639a62fb34aa8b5b92
frederic wagner <frederic.wagner@imag.fr>
2020-05-02 19:21:10 +0200

    home manager : gtk+qt

--- modules/home.nix
+++ modules/home.nix
@@ -1,6 +1,6 @@
 {
   home-manager.users.wagnerf = { pkgs, ... }: {
-    home.packages = [ pkgs.exa ];
+    home.packages = [ pkgs.exa pkgs.dconf ];
     programs.git = {
       enable = true;
       userName = "frederic wagner";
@@ -13,7 +13,7 @@
       defaultKeymap = "viins";
       oh-my-zsh = {
         enable = true;
-        plugins = [ "archlinux" "git-extras" "git" "gitfast" "github" ];
+        plugins = [ "git" "colored-man-pages" "colorize" "vi-mode" ];
         theme = "gnzh";
       };
       loginExtra = ''
@@ -22,6 +22,38 @@
         bindkey '^R' history-incremental-pattern-search-backward
         bindkey '^F' history-incremental-pattern-search-forward
       '';
+    };
+    gtk = {
+      enable = true;
+      font = {
+        name = "Noto Sans 14";
+        package = pkgs.noto-fonts;
+      };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.gnome3.adwaita-icon-theme;
+      };
+      theme = {
+        name = "Adapta-Nokto";
+        package = pkgs.adapta-gtk-theme;
+      };
+    };
+    qt = {
+      enable = true;
+      platformTheme = "gtk";
+    };
+
+    services.gnome-keyring.enable = true;
+    services.gpg-agent.enable = true;
+    services.network-manager-applet.enable = true;
+    services.flameshot.enable = true;
+    services.redshift = {
+      enable = true;
+      latitude = "45.1667";
+      longitude = "5.7167";
+      brightness.day = "1";
+      brightness.night = "0.6";
+      tray = true;
     };
   };
 
